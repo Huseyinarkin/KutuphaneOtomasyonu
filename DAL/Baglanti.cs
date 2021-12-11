b69a6674c4fa07c6d17253804e134e32a659cf3a
Huseyin ARKIN <99.arkn@gmail.com>
2021-12-11 14:38:22 +0300

    DAL created.

---
+++ DAL/Baglanti.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace DAL
+{
+    class Baglanti
+    {
+        //Veri tabanı ile bağlantı işlemi gerçekleştirildi
+        public static OleDbConnection baglanti = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\Database1.mdb");
+        public static void Connection(OleDbCommand command)
+        {
+
+            if (command.Connection.State != ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+        }
+    }
+}
