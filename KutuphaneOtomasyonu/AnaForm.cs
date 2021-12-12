70a6408f52c6b224fba54abb8813d6909291bbe0
Huseyin ARKIN <99.arkn@gmail.com>
2021-12-13 01:27:33 +0300

    Main design created.

---
+++ KutuphaneOtomasyonu/AnaForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BL;        // BL katmanını kullanacağımız için ekledik.
+using Entity;   // Entity katmanını kullanacağımız için ekledik.
+
+namespace KutuphaneOtomasyonu
+{
+    public partial class AnaForm : Form
+    {
+        public AnaForm()
+        {
+            InitializeComponent();
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
