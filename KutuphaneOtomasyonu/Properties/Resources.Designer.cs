cd4fe1df2efbafba7ea88fe31a8347a91e481c6e
Huseyin ARKIN <99.arkn@gmail.com>
2021-12-13 21:43:28 +0300

    Fixed.

--- KutuphaneOtomasyonu/Properties/Resources.Designer.cs
+++ KutuphaneOtomasyonu/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace KutuphaneOtomasyonu.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap final2 {
-            get {
-                object obj = ResourceManager.GetObject("final2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
